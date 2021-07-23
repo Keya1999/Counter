90f29effb952c2d97a78e4def46d235cdd42ace2
Keya Gandhi <keyag@mindfiresolutions.com>
2021-07-23 15:55:38 +0530

    removed unnecessary files

--- counter_frontend/src/component/counter.js
+++ counter_frontend/src/component/counter.js
@@ -1,43 +1,50 @@
-
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import Button from "./Button";
 
 const Counter = () => {
-    const [values,setValues] = useState({
-        counter_value : 1,
-        max_value : 1000
-    })
+  const [values, setValues] = useState({
+    counter_value: 1,
+    max_value: 1000,
+  });
 
-    const {counter_value,max_value} = values;
+  const { counter_value, max_value } = values;
 
-    const handleChange = (name) => event => {
-        console.log("name value : ",name + " " + event.target.value)
-        setValues({...values, [name]: parseInt(event.target.value)})
-    }
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, [name]: parseInt(event.target.value) });
+  };
 
-    const incrementCounter = () => {
-        console.log("increment")
-        if (counter_value === null || Number.isNaN(counter_value))
-            setValues({...values,counter_value : parseInt(1)})
-        else
-            setValues({...values,counter_value : parseInt(counter_value + 1)})
-        
+  const incrementCounter = () => {
+    if (counter_value === null || Number.isNaN(counter_value))
+      setValues({ ...values, counter_value: parseInt(1) });
+    else setValues({ ...values, counter_value: parseInt(counter_value + 1) });
+  };
 
-    }
+  const decrementCounter = () => {
+    setValues({ counter_value: counter_value - 1 });
+  };
 
-    const decrementCounter = () => {
-        console.log("decrement")
-        setValues({counter_value : counter_value - 1})
-    }
+  const displayCounter = () => {
+    return (
+      <React.Fragment>
+        <div>
+          <Button onClick={decrementCounter}>-</Button>
+          <input
+            type="number"
+            value={counter_value}
+            name="counter_value"
+            onChange={handleChange("counter_value")}
+          />
+          <Button
+            onClick={incrementCounter}
+            disabled={counter_value === max_value}
+          >
+            +
+          </Button>
+        </div>
+      </React.Fragment>
+    );
+  };
+  return <div>{displayCounter()}</div>;
+};
 
-    const displayCounter = () => {
-        return (<React.Fragment><div><button onClick={decrementCounter}>-</button>
-                <input type="number" value={counter_value} name="counter_value" onChange={handleChange('counter_value')}/>
-                <button onClick={incrementCounter} disabled={counter_value===max_value}>+</button></div>
-                <div>Maximum value : <input type="number" value={max_value} onChange={handleChange('max_value')}/></div>
-                </React.Fragment>
-            )
-    }
-    return <div>{displayCounter()}</div>
-}
-
-export default Counter
+export default Counter;
